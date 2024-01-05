eef072eea2a159eff9320f2b6293742af43b8ee4
Logimancer <the.logimancer@gmail.com>
2024-01-04 21:31:15 -0500

    Alpha Release 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//#![windows_subsystem = "windows"] //keeps a console from opening
+#![windows_subsystem = "windows"] //keeps a console from opening
 use windows::{Devices::{
     SerialCommunication::SerialDevice, 
     Enumeration::{
@@ -11,7 +11,8 @@ use windows::{Devices::{
 };
 use async_std::task;
 use tray_icon::{
-    menu::{Menu, MenuEvent, MenuItem, PredefinedMenuItem}, TrayIconBuilder, TrayIconEvent};
+    menu::{Menu, MenuEvent, MenuItem, PredefinedMenuItem, IsMenuItem}, 
+    TrayIconBuilder};
 use winit::event_loop::{ControlFlow, EventLoopBuilder};
 
 //Async wrapper for getting device info
@@ -36,7 +37,8 @@ fn get_serial_devices() -> DeviceInformationCollection  {
     let deviceid: windows::core::HSTRING = 
         SerialDevice::GetDeviceSelector().unwrap();
     //Get device information for name
-    let deviceinformation: Result<IAsyncOperation<DeviceInformationCollection>, Error> = 
+    let deviceinformation: 
+        Result<IAsyncOperation<DeviceInformationCollection>, Error> = 
         DeviceInformation::FindAllAsyncAqsFilter(&deviceid);
     let dev_info_collection: DeviceInformationCollection = 
         task::block_on(serial_ports_device_info(
@@ -44,6 +46,38 @@ fn get_serial_devices() -> DeviceInformationCollection  {
         )
     );
     dev_info_collection
+}
+
+fn refresh_serial_ports() -> Vec<MenuItem> {
+    let mut menu_items_ports: Vec<MenuItem> = Vec::new();
+    let serial_devices_information_collection: 
+        DeviceInformationCollection = get_serial_devices();
+    let serial_devices: Vec<(HSTRING, HSTRING)> = 
+        enumerate_serial_devices(
+            serial_devices_information_collection);
+    for serial_device in serial_devices {
+        //iterate through each serial device and add to menu
+        let current_menu_item = 
+            serial_device.0.to_string() + " " + &serial_device.1.to_string();
+        menu_items_ports.push(
+            MenuItem::new(current_menu_item, 
+            false,
+            Option::None));
+    }
+    menu_items_ports
+}
+
+fn enumerate_serial_devices(
+    serial_device_information_collection: DeviceInformationCollection) -> 
+    Vec<(HSTRING, HSTRING)> {    
+    let mut serial_devices:Vec<(HSTRING, HSTRING)> = Vec::new(); 
+    for serial_device in serial_device_information_collection {
+        let serial_device_id = serial_device_comm_number(
+            serial_device.Id().unwrap());
+        let serial_device_name = serial_device.Name().unwrap();
+        serial_devices.push((serial_device_id, serial_device_name));
+    }
+    serial_devices
 }
 
 fn serial_device_comm_number(deviceid: HSTRING) -> windows::core::HSTRING {
@@ -56,6 +90,23 @@ fn serial_device_comm_number(deviceid: HSTRING) -> windows::core::HSTRING {
     );
     let serial_return: HSTRING = serial_device.PortName().unwrap();
     serial_return 
+}
+    
+fn build_menu_ports(menu: &Menu, menu_items_ports: Vec<MenuItem>) -> Menu {
+    //Add Ports to menu
+    for port in menu_items_ports {
+        let _ = menu.prepend(&port);
+    }
+    menu.clone()
+}
+
+fn remove_current_menu_ports(menu: &Menu) {
+    let i = 1;
+    for item in menu.items() {
+        if item.as_menuitem().is_none() { break; }
+        println!("{}", i);
+        let _ = menu.remove(item.as_menuitem().unwrap());
+    }
 }
 
 fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
@@ -70,53 +121,18 @@ fn load_icon(path: &std::path::Path) -> tray_icon::Icon {
     tray_icon::Icon::from_rgba(icon_rgba, icon_width, icon_height).expect("Failed to open icon")
 }
 
-fn enumerate_serial_devices(serial_device_information_collection: DeviceInformationCollection) -> 
-    Vec<(HSTRING, HSTRING)> {    
-    let mut serial_devices:Vec<(HSTRING, HSTRING)> = Vec::new(); 
-    for serial_device in serial_device_information_collection {
-        let serial_device_id = serial_device_comm_number(
-            serial_device.Id().unwrap());
-        let serial_device_name = serial_device.Name().unwrap();
-        serial_devices.push((serial_device_id, serial_device_name));
-    }
-    serial_devices
-}
-
 fn main() {    
-
-    //enumerate serial devices
-    let mut number_of_serial_devices: i32 = 0;
-    let menu_items_ports: Vec<MenuItem> = Vec::new();
-    fn refresh_serial_ports(mut menu_items_ports: Vec<MenuItem>/* serial_device_information_collection: DeviceInformationCollection*/)  
-        -> Vec<MenuItem> {
-        let serial_devices_information_collection: DeviceInformationCollection = 
-        get_serial_devices();
-        let serial_devices: Vec<(HSTRING, HSTRING)> = 
-        enumerate_serial_devices(serial_devices_information_collection);
-        for serial_device in serial_devices {
-            //iterate through each serial device and add to menu
-            //println!("{} {}", serial_device.0, serial_device.1);
-            let current_menu_item = serial_device.0.to_string() + " " + &serial_device.1.to_string();
-            menu_items_ports.push(MenuItem::new(current_menu_item, false, Option::None));
-            //number_of_serial_devices = number_of_serial_devices + 1;
-        }
-        menu_items_ports
-    }
-    //let number_of_serial_devices_str: String = 
-    //    "Number of ports: ".to_owned() + &number_of_serial_devices.to_string();
-    //setup icon for tray
+    //setup tray icon
     let path: &str = "icon\\icon.ico";
     let icon: tray_icon::Icon = load_icon(std::path::Path::new(path));
     
     //build event loop for tray (req'd)
-    let event_loop: winit::event_loop::EventLoop<()> = EventLoopBuilder::new().build().unwrap();
+    let event_loop: winit::event_loop::EventLoop<()> = 
+        EventLoopBuilder::new().build().unwrap();
     
-    //Show comports in menu, iterating through list made above:
-        
     //Setup tray menu and tray menu items
-    //TODO: make this a function where you pass in the menu struct, it clears it and adds new
-    let menu: Menu = Menu::new();    
-     //Seperator definition
+    let menu: Menu = Menu::new();
+    //Seperator definition
     let menu_item_seperator = PredefinedMenuItem::separator();
     //Refresh definition
     let menu_item_refresh = MenuItem::new(
@@ -125,51 +141,38 @@ fn main() {
     let menu_item_quit: MenuItem = MenuItem::new(
             "Quit", true, Option::None);
     //Add control items after ports to menu
-    let _ = menu.append_items(&[&menu_item_seperator, 
-                                    &menu_item_refresh,
-                                    &menu_item_seperator,
-                                    &menu_item_quit]);   
-    fn build_menu_ports(menu: Menu, menu_items_ports: Vec<MenuItem>) -> Menu {
-        //Add Ports to menu
-        for port in menu_items_ports {
-            let _ = menu.prepend(&port);    
-        }
-        menu
-    }
-    fn remove_current_menu_ports(menu: Menu, mut menu_items_ports: Vec<MenuItem>) {
-        //remove ports
-        for port in &menu_items_ports {
-            let _ = menu.remove(&*port);
-        }
-        menu_items_ports.clear();
-        let _ = build_menu_ports(menu, refresh_serial_ports(menu_items_ports));
-    }
-    let menu_items_ports = refresh_serial_ports(menu_items_ports);
-    let _ = build_menu_ports(menu.clone(), menu_items_ports.clone());
-    
+    let persistant_menu_items: [&dyn IsMenuItem; 4] = [&menu_item_seperator, 
+                                                       &menu_item_refresh,
+                                                       &menu_item_seperator,
+                                                       &menu_item_quit];   
+        
+    //build initial serial port portion of menu
+    let menu: Menu = build_menu_ports(&menu, refresh_serial_ports());
+    //add persistent control portion of menu
+    let _ = menu.append_items(&persistant_menu_items);
+
     //display tray icon
     let _tray_icon = Some(
         TrayIconBuilder::new()
             .with_menu(Box::new(menu.clone()))
-            .with_tooltip("Serial Ports"/*&number_of_serial_devices_str*/) //number of comports on hover
+            .with_tooltip("Serial Port Status") 
             .with_icon(icon)
             .build()
             .unwrap(),
     );
+
+    //event loop logic
     let menu_channel = MenuEvent::receiver();
-    let tray_channel = TrayIconEvent::receiver();
     let _ = event_loop.run(move |_event: winit::event::Event<()>, 
         event_loop: &winit::event_loop::EventLoopWindowTarget<()>| {
         event_loop.set_control_flow(ControlFlow::Poll);
-        if let Ok(event) = tray_channel.try_recv() {
-            println!("{event:?}");
-        }
         if let Ok(event) = menu_channel.try_recv() {
             println!("{event:?}");
             
             //Refresh action
             if event.id.0 == menu_item_refresh.id().0 {
-                let _ = remove_current_menu_ports(menu.clone(), menu_items_ports.clone());    
+                remove_current_menu_ports(&menu);
+                build_menu_ports(&menu, refresh_serial_ports());
             }
             //Quit action
             if event.id.0 == menu_item_quit.id().0 {
